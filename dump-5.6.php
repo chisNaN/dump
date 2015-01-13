0bbc2d65cce0b35c7558eddc773e298a86387da3
agreg0ire <arthurart85@msn.com>
2015-01-13 17:15:50 +0100

    - added features for php 5.6

---
+++ dump-5.6.php
@@ -0,0 +1,136 @@
+<?php
+
+class Dump
+{
+
+    public function __construct()
+    {
+        if(version_compare(PHP_VERSION, '5.6.0', '<'))
+        {
+            die('You need at least PHP version 5.6 to execute this class');
+        }
+    }
+
+    protected function dumped($p1_m_var)
+    {
+        static $i_num_imbricated_array, $i_num_obj;
+
+        if($p1_m_var === $GLOBALS) die('<b>WARNING :: dumping $GLOBALS will lead to infinite loop!</b><br/>');
+
+        if(is_null($p1_m_var))
+        {
+            yield '<b>NULL</b>';
+
+        }elseif(is_string($p1_m_var))
+        {
+            if(is_callable($p1_m_var, FALSE, $s_callable_name))
+            {
+
+                yield '<font color="#ccc">CALLABLE</font> of name ('.$s_callable_name.')';
+
+            }else yield '<font color="blue">STRING</font> ('.strlen($p1_m_var).') "' . $p1_m_var . '"';
+
+        }elseif(is_bool($p1_m_var))
+        {
+
+            if($p1_m_var)
+            {
+                yield '<font color="green">BOOL</font> (true)';
+
+            }else yield '<font color="green">BOOL</font> (false)';
+
+        }elseif(is_int($p1_m_var))
+        {
+            yield '<font color="MediumSpringGreen" >INT</font> ('.$p1_m_var.')';
+
+        }elseif(is_float($p1_m_var))
+        {
+            yield '<font color="orange">FLOAT</font> ('.$p1_m_var.')';
+
+        }elseif(is_resource($p1_m_var))
+        {
+
+            yield '<font color="purple">RESOURCE</font> of type ('.get_resource_type($p1_m_var).')';
+
+        }elseif(is_object($p1_m_var))
+        {
+            $i_num_obj++;
+
+            yield '<span  style="color: #800000; cursor: pointer;"
+		onclick="(document.getElementById(\'obj_'.$i_num_obj.'\').style.display == \'block\') ? document.getElementById(\'obj_'.$i_num_obj.'\').style.display = \'none\' : document.getElementById(\'obj_'.$i_num_obj.'\').style.display = \'block\';" >
+					            OBJECT :: '.get_class($p1_m_var).'</span>';
+
+            yield '<ul id="obj_'.$i_num_obj.'" style="display: none; list-style-type: none; margin-top: 0px;">';
+
+            yield preg_replace('#(^Class )|(\s{1}Constants \[\d+\])|(Static properties \[\d+\])|(\s{1}Properties \[\d+\])|(Static methods \[\d+\])|(\s{1}Methods \[\d+\])#',
+                '<ul><font color=red>${2}${3}${4}${5}${6}</font></ul>',
+                ReflectionClass::export($p1_m_var, TRUE));
+
+            yield '</ul>';
+
+        }elseif(is_array($p1_m_var))
+        {
+            $i_num_imbricated_array++;
+
+            yield '<span  style="color: #FF0000; cursor: pointer;"
+			 					onclick="(document.getElementById(\'level_'.$i_num_imbricated_array.'\').style.display == \'block\') ? document.getElementById(\'level_'.$i_num_imbricated_array.'\').style.display = \'none\' : document.getElementById(\'level_'.$i_num_imbricated_array.'\').style.display = \'block\';">
+								 ARRAY</span> ('.count($p1_m_var).')
+								<ul style="list-style-type: none; display: none; margin-top: 0px;" id="level_'.$i_num_imbricated_array.'"> { ';
+
+            foreach($p1_m_var as $m_key => $m_value):
+
+                if(is_int($m_key))
+                {
+                    yield '<li>&nbsp;&nbsp;['.$m_key.'] => ';
+
+                    foreach($this->dumped($m_value) as $new_call) yield $new_call;
+
+                    yield '</li>';
+
+                }elseif(is_string($m_key))
+                {
+                    yield '<li>&nbsp;&nbsp;["'.$m_key.'"] => ';
+
+                    foreach($this->dumped($m_value) as $new_call) yield $new_call;
+
+                    yield '</li>';
+
+
+                }else foreach($this->dumped($m_value) as $new_call) yield $new_call;
+
+            endforeach;
+
+            yield ' } </ul>';
+        }
+    }
+
+    public function displayHTML($p1_m_var, ...$p2_additionals_params)
+    {
+        if(count($p2_additionals_params) > 0) die('<b>Error: You try to pass more than one var ! </b><br /> ');
+
+        $s_html_dump = '';
+
+        foreach($this->dumped($p1_m_var) as $v) $s_html_dump .= $v;
+
+        echo $s_html_dump;
+    }
+
+    public function writeHTML($p1_m_var, $p2_filename, ...$p3_additionals_params)
+    {
+        if(count($p3_additionals_params) > 0) die('<b>Error: You try to pass more than one var ! </b><br /> ');
+
+        $s_html_nodes = '';
+
+        foreach($this->dumped($p1_m_var) as $v) $s_html_nodes .= $v;
+
+        $html = '<!doctype html><html lang="en"><head><meta charset="utf-8"></head><body>'.$s_html_nodes.'</body></html>';
+
+        if(FALSE === @file_put_contents($p2_filename.'.htm', $html))
+        {
+            return '<b>Error: unable to write file, please check path or write permissions</b>';
+
+        }else return TRUE;
+
+    }
+
+}
